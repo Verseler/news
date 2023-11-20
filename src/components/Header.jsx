c56b882717466348bfa7bab9474d9bc4efd75ed0
verseler <handuman.verselerkerr@gmail.com>
2023-11-20 18:38:52 +0800

    fix: header bg color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header(props) {
   let [showSideMenu, setSideMenu] = useState(true);
 
   return (
-    <header className="fixed inset-x-0 top-0 h-24 pt-8 dark:bg-dark-gray">
+    <header className="fixed inset-x-0 top-0 h-24 pt-8 bg-white dark:bg-dark-gray">
       <div className="flex items-center justify-between h-full primary-container">
         <div>
           <img className="object-contain max-h-7" src={logo} />
@@ -21,13 +21,13 @@ export default function Header(props) {
           }}
         >
           <img
-            className="object-contain fill-white"
+            className="object-contain"
             src={showSideMenu ? openMenuBtn : closeMenuBtn}
           />
         </div>
         <nav
           className={`${
-            showSideMenu ? "hidden" : "block"
+            showSideMenu ? "hidden" : "block" 
           } fixed h-full sm:h-auto sm:static sm:block inset-x-0  bg-gray-600/50 top-24`}
         >
           <ul className="flex flex-col w-3/4 h-full gap-8 p-16 ml-auto bg-white dark:bg-dark-gray dark:text-white first:text-sm sm:flex-row sm:w-full sm:p-0">
